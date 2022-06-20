9dc8b3c6cb23bd07fe5e37ba28ce39b5a26ef646
thangnx183 <nxthang881972@gmail.com>
2022-06-20 03:47:20 +0000

    add wandb hook

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -1,5 +1,7 @@
 from .collect_env import collect_env
 from .logger import get_root_logger,log_img_scale
 from .optimizer import DistOptimizerHook
+from .wandblogger_hook import MMDetWandbHook
 
-__all__ = ['get_root_logger', 'collect_env', 'DistOptimizerHook','log_img_scale']
+__all__ = ['get_root_logger', 'collect_env', 'DistOptimizerHook','log_img_scale','MMDetWandbHook']
+ 
